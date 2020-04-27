0490d08a94cce1ff2df3833ac52c4fb55456d6a4
mikevampm323 <mike.vartanyan@yahoo.com>
2020-04-26 17:55:11 -0700

    added boilerplate

--- server.js
+++ server.js
@@ -12,14 +12,15 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
-
-
 });
 
 app.listen(3000, () => {
